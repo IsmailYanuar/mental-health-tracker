c1ec9cada394a0184a1b7734372fb20de65d6c6b
IsmailYanuar <ismailyanuar1960@gmail.com>
2024-10-02 16:35:04 +0700

    Tutorial 5 PBP

--- main/views.py
+++ main/views.py
@@ -11,18 +11,20 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect    
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+
 
 # Create your views here.
 
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
 
     context = {
         'name': request.user.username,
         'class': 'PBP E',
         'npm': '2306245781',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -64,9 +66,10 @@ def login_user(request):
         return response
 
    else:
+      messages.error(request, "Invalid username or password. Please try again.")
       form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+      context = {'form': form}
+      return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -97,12 +100,29 @@ def delete_mood(request, id):
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
 
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) # strip HTML tags!
+    feelings = strip_tags(request.POST.get("feelings")) # strip HTML tags!
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
+
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
